fbf2ebd0e22d160fbef82e6aabf223f156d29c10
KilianCorbel <kilian-corbel@live.fr>
2018-04-22 18:39:37 +0200

    sha1, sha256, md5

--- blockfiles.py
+++ blockfiles.py
@@ -11,13 +11,33 @@ DATABASE = "blockfiles.db"
 BUF_SIZE = 65536
 
 # Hasher
-hasher = hashlib.sha256()
+sha256 = hashlib.sha256()
+md5 = hashlib.md5()
+sha1 = hashlib.sha1()
+
+def hashAndInsert(root, hasher):
+	print("Hashing function : ",hasher)
+	# Loop on files in selected folder and sub-folders
+	for root, dirs, filenames in os.walk(root):
+		for f in filenames:
+			# Open file in binary mode
+			log = open(os.path.join(root, f), 'rb')
+			while True:
+				data = log.read();
+				if not data:
+					break
+				# Get file hash
+				hasher.update(data)
+				# Insert new row for file
+				hash = format(hasher.hexdigest())
+				date = format(time.time())
+				c.execute("INSERT INTO files VALUES(?,?,?)", (f, hash, date,))
 
 # Window
 #window = Tk()
 #window.withdraw()
-c = 0
 
+# Check if Database exists
 if os.path.isfile(DATABASE):
 	# Database Init
 	conn = sqlite3.connect("blockfiles.db")
@@ -33,35 +53,30 @@ else:
 # Open filedialog
 root = filedialog.askdirectory()
 print("\nRoot directory : ", root)
+
 # Start timer by retrieving current time
 start = time.time()
 
-# Loop on files in selected folder and sub-folders
-for root, dirs, filenames in os.walk(root):
-	for f in filenames:
-		# Open file in binary mode
-		log = open(os.path.join(root, f), 'rb')
-		while True:
-			data = log.read(BUF_SIZE);
-			if not data:
-				break
-			# Get file hash
-			hasher.update(data)
-			# Insert new row for file
-			file = [(f, format(hasher.hexdigest()), time.time())]
-			print(file)
-			c.executemany("INSERT INTO files VALUES(?,?,?)", file)
+# Hash and insert files
+hashAndInsert(root, sha256)
 
 # End timer by retrieving current time
 finish = time.time()
 # Substract
 timeElapsed = finish-start
 
-print("\nResults :\n")
-for row in c.execute("SELECT * FROM files"):
-	print(row)
+#print("\nResults :\n")
+#c.execute("SELECT * FROM files")
+#r = c.fetchall()
+#for value in r:
+ #   print(value[1])
 
 print("\nDone")
 print("Time elapsed :", timeElapsed)
+
+# Commit changes to db and close it
+conn.commit()
+conn.close()
+
 
 
